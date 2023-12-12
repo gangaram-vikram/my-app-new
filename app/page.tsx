a0a9e9bd78671e394b0898267deff85a75c8d223
gangaram-vikram <gangaramvikramvicky@gmail.com>
2023-12-12 21:29:10 +0530

    ext-overflow: ellipsis;

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className='text-7xl	text-red-500	'>Suruchi-Cafe Kanha Shanti Vanam </h1>
+     <h1 className='text-7xl	text-red-500  ext-overflow: ellipsis;	'>Suruchi-Cafe Kanha Shanti Vanam </h1>
+
      
        
     </main>
