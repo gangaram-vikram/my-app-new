9ff0feebab983c723609f894a4d5bbd87939f420
gangaram-vikram <gangaramvikramvicky@gmail.com>
2023-12-12 21:51:03 +0530

    /* 18px */     line-height: 1.75rem; /* 28px */ flex-col items-center justify-between p-24

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen  font-size: 1.125rem; /* 18px */
-    line-height: 1.75rem; /* 28px */ flex-col items-center justify-between p-24">
-     <h1 className='text-7xl	text-red-500  ext-overflow: ellipsis;	'>Suruchi-Cafe Kanha Shanti Vanam </h1>
+    <main className="flex min-h-screen  font-size: 1.125rem; ">
+     <h1 className='/* 18px */
+    line-height: 1.75rem; /* 28px */ flex-col items-center justify-between p-24	text-red-500  ext-overflow: ellipsis;	'>Suruchi-Cafe Kanha Shanti Vanam </h1>
 
      
        
