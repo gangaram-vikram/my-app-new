1c5ec4e2d55e351fddc75e660ef6f6fb706bcc03
gangaram-vikram <gangaramvikramvicky@gmail.com>
2024-01-04 15:01:42 +0530

    module.exports = {   images: {     remotePatterns: [       {         protocol: 'https',         hostname: 's3.amazonaws.com',         port: '',         pathname: '/my-bucket/**',       },     ],   }, }

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = nextConfigmodule.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 's3.amazonaws.com',
+          port: '',
+          pathname: '/my-bucket/**',
+        },
+      ],
+    },
+  }
