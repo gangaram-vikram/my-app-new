67e6f6772094650c293b8a9c1703a3cd98e5569d
gangaram-vikram <gangaramvikramvicky@gmail.com>
2024-01-11 11:34:14 +0530

    Albetross

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen  font-size: 1.125rem; ">
      <h1 className='/* 18px */
-    line-height: 1.75rem; /* 28px */ flex-col items-center justify-between p-24	text-red-500  ext-overflow: ellipsis;	'>Gangarapu Vikram </h1>
+    line-height: 1.75rem; /* 28px */ flex-col items-center justify-between p-24	text-red-500  ext-overflow: ellipsis;	'>Albetross </h1>
 
      
        
--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfigmodule.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 's3.amazonaws.com',
-          port: '',
-          pathname: '/my-bucket/**',
-        },
-      ],
-    },
-  }
+module.exports = nextConfig
